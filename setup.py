72cf5f80db2bbc0a103d2b558888758f8bc6d731
SimiPixel <61733064+SimiPixel@users.noreply.github.com>
2021-10-27 15:45:40 +0200

    Update setup.py
    typo

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name="mat2py",
     version="0.0.1",
-    py_modules=["mat2py"],
+    py_modules=["mat2py.mat2py"],
     install_requires=["numpy", "scipy", "pathlib"]
 )
